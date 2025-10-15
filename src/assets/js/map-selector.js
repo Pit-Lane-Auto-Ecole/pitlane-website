e259c026b0d9f705f3a836d3ae177d2d709c2c7c
dieperid <david.dieperink@ik.me>
2025-10-15 21:40:59 +0200

    fix: fixed navbar dropdown bug

--- src/assets/js/map-selector.js
+++ src/assets/js/map-selector.js
@@ -1,12 +1,3 @@
-function toggleDropdown(id) {
-    const dropdown = document.getElementById(id);
-    if (dropdown.classList.contains("hidden")) {
-        dropdown.classList.remove("hidden");
-    } else {
-        dropdown.classList.add("hidden");
-    }
-}
-
 document.getElementById("mapSelector").addEventListener("change", function () {
     const selectedMapId = this.value;
 
